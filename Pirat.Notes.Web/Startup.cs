c5e1c2c8657292519ff2ce6a8c178c1e9b49b79f
Pirat <intake@list.ru>
2023-05-25 02:16:15 +0300

    Configure application for using SwaggerUI

--- Pirat.Notes.Web/Startup.cs
+++ Pirat.Notes.Web/Startup.cs
@@ -5,12 +5,23 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Pirat.Notes.DAL.Implementations;
 using Pirat.Notes.DAL.Implementations.Extensions;
 using Pirat.Notes.Domain.Implementations.Extensions;
 using Pirat.Notes.Shared.Authorization;
 using Pirat.Notes.Shared.Helpers;
 using Serilog;
+using System.Collections.Generic;
+
+using Swashbuckle;
+using Swashbuckle.AspNetCore;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using System.Reflection;
+using Microsoft.Extensions.Options;
+using System.IO;
+using System;
 
 namespace Pirat.Notes.Web
 {
@@ -37,6 +48,41 @@ namespace Pirat.Notes.Web
 
             services.AddControllers();
 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Pirat.Notes API" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
+
+            });
+
             services.AddHttpContextAccessor();
 
             services.AddAutoMapper(typeof(Startup));
@@ -55,6 +101,8 @@ namespace Pirat.Notes.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pirat.Notes.Web v1"));
             }
             else
             {
